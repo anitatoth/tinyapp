d474a417dfb6f2365219bdc5a581b730b68ce2b5
Anita Toth <haluska99@hotmail.com>
2021-02-15 23:42:59 +0000

    updated form to redirect user to new generated short URL site

--- express_server.js
+++ express_server.js
@@ -14,10 +14,9 @@ function generateRandomString(length) {
   return result;
 }
 
-generateRandomString(6);
 
 
-console.log(makeid(5));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -26,9 +25,17 @@ const urlDatabase = {
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-function generateRandomString() {
-
+function generateRandomString(length) {
+  let result = '';
+  let char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+  let charLength = char.length
+  for ( let i = 0; i < length; i++) {
+    result += char.charAt(Math.floor(Math.random() * charLength));
+  }
+  return result;
 }
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -57,8 +64,9 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+ const shortURL = generateRandomString(6);
+ urlDatabase[shortURL] = req.body.longURL;
+ res.redirect(`/urls/:${shortURL}`)
 });
 
 

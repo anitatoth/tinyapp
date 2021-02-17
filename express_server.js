7902b5d50712eb5ff1a4eb0bee3cea9d72a21d8b
Anita Toth <haluska99@hotmail.com>
2021-02-17 19:58:55 +0000

    created registration page ejs template and added post register route to express server

--- express_server.js
+++ express_server.js
@@ -42,6 +42,17 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.post("/login", (req, res) => {
+  const usernameSubmitted = req.body.username;
+  console.log(req.body)
+  res.cookie("username", usernameSubmitted);
+  res.redirect('/urls');
+ });
+
 app.get("/urls/new", (req, res) => {
   const templateVars = {
     username: req.cookies["username"]}
@@ -52,22 +63,26 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
+
+
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
-app.post("/login", (req, res) => {
-  const usernameSubmitted = req.body.username;
-  console.log(req.body)
-  res.cookie("username", usernameSubmitted);
-  res.redirect('/urls');
- });
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
+
  
  app.post("/logout", (req, res) => {
   res.clearCookie('username');
@@ -81,10 +96,7 @@ app.post("/urls", (req, res) => {
  res.redirect(`/urls/${shortURL}`)
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
-  res.redirect(longURL);
-});
+
 
 // Delete  POST /urls/:shortURL/delete
 app.post('/urls/:shortURL/delete', (req, res) => {
